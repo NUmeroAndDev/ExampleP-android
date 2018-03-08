45d4b95c76e16a5420dc57050881c857803ea693
NUmeroAndDev <numero.dev.and@gmail.com>
2018-03-08 12:17:51 +0900

    BottomAppBarの追加

---
+++ app/src/main/java/com/numero/android_p_example/BottomAppBarActivity.kt
@@ -0,0 +1,84 @@
+package com.numero.android_p_example
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_bottom_app_bar.*
+import kotlinx.android.synthetic.main.app_bar_bottom_app_bar.*
+
+class BottomAppBarActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, Toolbar.OnMenuItemClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_app_bar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar)
+        bottomAppBar.setOnMenuItemClickListener(this)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, bottomAppBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        item ?: return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    companion object {
+        fun createIntent(context: Context): Intent = Intent(context, BottomAppBarActivity::class.java)
+    }
+}
--- app/src/main/java/com/numero/android_p_example/MainActivity.kt
+++ app/src/main/java/com/numero/android_p_example/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.numero.android_p_example
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomAppBarCardView.setOnClickListener {
+            startActivity(BottomAppBarActivity.createIntent(this@MainActivity))
+        }
     }
 }
